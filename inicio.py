92153c7b081a350ee5fde1a734d736f4cd0c087f
luccas neves palhares <luccas_neves@hotmail.com>
2021-03-23 18:08:33 -0300

    stud

---
+++ inicio.py
@@ -0,0 +1,34 @@
+print('Oi ' 'ok')
+#coments
+#operações matematicas
+print(9+9)
+print(9-9)
+print(9/9)
+print (9**9)
+
+#parte inteira da divisão
+print(9//2)
+#resto de divisão
+print (9%4)
+
+#atribuindo valor a variavel usando =
+a = 9
+#se for igual ==
+4 == 6
+
+b = 7
+c = a + b
+print(c)
+ variavel1 = 8
+ variavel2 = 4
+ variavel1 = 7
+ print(variavel1)
+ b = 12
+ # as variaveis podem mudar confome o tempo
+
+ var = 45
+ var2 = 43
+  type(var)
+  palavra = 'Dia de Sol'
+  print(var + var2)
+  
