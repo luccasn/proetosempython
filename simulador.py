8d80dceb671320f1ae66e953e680d0f28645a8f0
luccas neves palhares <luccas_neves@hotmail.com>
2021-03-23 17:10:49 -0300

    testando

--- simulador.py
+++ simulador.py
@@ -1,17 +1,29 @@
 #simular o uso de um dado
 import random
+import PySimpleGUI
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'voce gostaria de jogar novamente'
+        layout = ´[
+            [sg.]
+        ]
 
         def Iniciar(self):
             resposta = input(self.mensagem)
-            if resposta == 'sim':
-                self.GerarValorDoDado()
+            try:
 
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado()
+                elif resposta == 'não' or resposta == 'n':
+                    print('Obrigado')
+
+                else:
+                    print('Digitar sim ou não')
+                except:
+                    print('ocorreu um erro na resposta')
 
         def GerarValorDoDado(self):
             print(random.randint(self.valorminimo,self.valor_maximo))
